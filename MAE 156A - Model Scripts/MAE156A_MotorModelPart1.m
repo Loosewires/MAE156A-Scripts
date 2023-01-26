2245ec977110c8d7c917163893049fcebb9d7ce2
Loosewires <giordanoliska@gmail.com>
2023-01-25 22:57:45 -0800

    update

--- MAE 156A - Model Scripts/MAE156A_MotorModelPart1.m
+++ MAE 156A - Model Scripts/MAE156A_MotorModelPart1.m
@@ -19,7 +19,7 @@ clc
         Tau_s_11 = K/R*(12*.5); % Stall torque [Nm]
         W_nl_11 = 12*.5/K; % No load speed [rad/s]
         J_11 = 7.34e-6; % Config 1 rotational inertia [kgm^2]
-        Tau_fm_11 = 5.86e-4; % Motor torque required to overcome friction [Nm]
+        Tau_fm_11 = 4.69e-4; % Motor torque required to overcome friction [Nm]
         W_nl_11 = W_nl_11*(Tau_s_11-Tau_fm_11)/Tau_s_11; % No load speed with friction [rad/s]
         TC_11 = W_nl_11 * J_11 / Tau_s_11; % Config 1 time constant
         W_11 = @(t) W_nl_11*(1-exp(-t/TC_11))*60/(2*pi); % Closed form solution
@@ -45,7 +45,7 @@ clc
         plot(t11, Vf11*60/(2*pi), Color = [0 0 1]);
         title('Config 1.1: Theoretical vs. Experimental');
         xlabel('Time (sec)');
-        ylabel('Filtered Velocity (RPM)');
+        ylabel('Filtered Velocity [Tf = 0.05] (RPM)');
         legend('Theoretical', 'Experimental', Location = 'southeast');
         axis([0 5 0 4500]);
 
@@ -129,7 +129,7 @@ clc
         Tau_s_21 = K/R*(12*.5); % Stall torque [Nm]
         W_nl_21 = 12*.5/K; % No load speed [rad/s]
         J_21 = 1.03e-5; % Config 1 rotational inertia [kgm^2]
-        Tau_fm_21 = 7.42e-4; % Motor torque required to overcome friction [Nm]
+        Tau_fm_21 = 5.94e-4; % Motor torque required to overcome friction [Nm]
         W_nl_21 = W_nl_21*(Tau_s_21-Tau_fm_21)/Tau_s_21; % No load speed with friction [rad/s]
         TC_21 = W_nl_21 * J_21 / Tau_s_21; % Config 1 time constant
         W_21 = @(t) W_nl_21*(1-exp(-t/TC_21))*60/(2*pi); % Closed form solution
@@ -192,7 +192,7 @@ clc
         plot(t22, Vf22*60/(2*pi), Color = [0 0 1]);
         title('Config 2.2: Theoretical vs. Experimental');
         xlabel('Time (sec)');
-        ylabel('Filtered Velocity (RPM)');
+        ylabel('Filtered Velocity [Tf = 0.05] (RPM)');
         legend('Theoretical', 'Experimental', Location = 'southeast');
         axis([0 5 0 6200]);
 
@@ -240,7 +240,7 @@ clc
         Tau_s_31 = K/R*(12*.5); % Stall torque [Nm]
         W_nl_31 = 12*.5/K; % No load speed [rad/s]
         J_31 = 1.95e-5; % Config 3.1 rotational inertia [kgm^2]
-        Tau_fm_31 = 1.24e-3; % Motor torque required to overcome friction [Nm]
+        Tau_fm_31 = 9.89e-4; % Motor torque required to overcome friction [Nm]
         W_nl_31 = W_nl_31*(Tau_s_31-Tau_fm_31)/Tau_s_31; % No load speed with friction [rad/s]
         TC_31 = W_nl_31 * J_31 / Tau_s_31; % Config 3.1 time constant
         W_31 = @(t) W_nl_31*(1-exp(-t/TC_31))*60/(2*pi); % Closed form solution
@@ -261,12 +261,12 @@ clc
 
         % Plot
         figure(7)
-        fplot(W_31, [0 15], Color = [.9 0 .1]);
+        fplot(W_31, [0 10], Color = [.9 0 .1]);
         hold on
         plot(t31, Vf31*60/(2*pi), Color = [0 0 1]);
         title('Config 3.1: Theoretical vs. Experimental');
         xlabel('Time (sec)');
-        ylabel('Filtered Velocity (RPM)');
+        ylabel('Filtered Velocity [Tf = 0.05] (RPM)');
         legend('Theoretical', 'Experimental', Location = 'southeast');
         axis([0 10 0 4500]);
 
@@ -338,7 +338,7 @@ clc
         plot(t33, Vf33*60/(2*pi), Color = [0 0 1]);
         title('Config 3.3: Theoretical vs. Experimental');
         xlabel('Time (sec)');
-        ylabel('Filtered Velocity (RPM)');
+        ylabel('Filtered Velocity [Tf = 0.05] (RPM)');
         legend('Theoretical', 'Experimental', Location = 'southeast');
         axis([0 10 0 8200]);
 
